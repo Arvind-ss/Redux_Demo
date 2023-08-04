435e55757dd8fa9ed1467d4b0bd75b7fad8ae4b2
Arvind-ss <128106810+Arvind-ss@users.noreply.github.com>
2023-08-04 19:36:28 +0530

    04-08-23

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const redux = require("redux");
 const createStore = redux.legacy_createStore;
+const combineReducers=redux.combineReducers 
 const CAKE_ORDERED = "cake_ordered";
-const CAKE_RESTOCKED="cake_restocked";
+const CAKE_RESTOCKED = "cake_restocked";
+const ICECREAM_ORDERED='iceCream_ordered';
+const ICECREAM_RESTOCKED='iceCream_restocked';
 //ACTIONS:-
 // * actions are the only way applications can interact with the store
 // * carry some info from app to redux store
@@ -10,48 +13,88 @@ const CAKE_RESTOCKED="cake_restocked";
 
 // //ActionCreator:a function which returns a action
 
-function order_cake(qty) {
+function cake_ordered(qty) {
   return {
     //Action is passed as a method
     type: "CAKE_ORDERED",
     payload: qty,
   };
 }
-function order_restocked(qty){
-  return{
-    type:"CAKE_RESTOCKED",
-    payload:qty
-  }
+function cake_restocked(qty) {
+  return {
+    type: "CAKE_RESTOCKED",
+    payload: qty,
+  };
+}
+function iceCream_ordered(qty) {
+  return {
+    type: "ICECREAM_ORDERED",
+    payload: qty,
+  };
+}
+function iceCream_restocked(qty) {
+  return {
+    type: "ICECREAM_RESTOCKED",
+    payload: qty,
+  };
 }
 // //State
-const initialState = {
+// const initialState = {
+//   noOfCakes: 10,
+// };
+const cake_initialState = {
   noOfCakes: 10,
+};
+const iceCream_initialState = {
+  noOficeCreams: 10,
 };
 // //Reducer:-
 // // * Reducer's Specify how the app's state changes inresponse to actions sent to the store
 // // * Reducer is a Function which accepts state and function as argument and returns thre next state as a
 // Reducer:- (previousState,action)=>new State
-const reducer = (state = initialState, action) => {
+const cake_reducer = (state = cake_initialState, action) => {
   switch (action.type) {
     case "CAKE_ORDERED":
       return {
         ...state,
         noOfCakes: state.noOfCakes - action.payload,
       };
-      case"CAKE_RESTOCKED":
-        return{
-          ...state,
-          noOfCakes:state.noOfCakes+action.payload
-        }
+    case "CAKE_RESTOCKED":
+      return {
+        ...state,
+        noOfCakes: state.noOfCakes + action.payload,
+      };
     default:
       return state;
   }
 };
-const store = createStore(reducer); // store created and the initialStates have been saved in the store
+const iceCream_reducer = (state = iceCream_initialState, action) => {
+  switch (action.type) {
+    case "ICECREAM_ORDERED":
+      return {
+        ...state,
+        noOficeCreams: state.noOficeCreams - action.payload,
+      };
+    case "ICECREAM_RESTOCKED":
+      return {
+        ...state,
+        noOficeCreams: state.noOficeCreams + action.payload,
+      };
+    default:
+      return state;
+  }
+};
+const rootReducer=combineReducers({
+  cake:cake_reducer,
+  iceCream:iceCream_reducer
+})
+const store = createStore(rootReducer); // store created and the initialStates have been saved in the store
 console.log("Initial State:", store.getState()); //since we have not performed any transsitions yet getState would provide
 const unSubscribe = store.subscribe(() =>
   console.log("Update State:", store.getState())
 );
-store.dispatch(order_cake(2));
-store.dispatch(order_restocked(1))
+store.dispatch(cake_ordered(2));
+store.dispatch(cake_restocked(1));
+store.dispatch(iceCream_ordered(2));
+store.dispatch(iceCream_restocked(1));
 unSubscribe();
---
+++ nested-state.js
@@ -0,0 +1,43 @@
+const redux = require("redux");
+const produce=require("immer.produce");
+//state
+const initialState={
+    name:"Aravind",
+    address:{
+        street:"123 Main st",
+        city:"Chennai",
+        State:"Tamil Nadu"
+    }
+}
+const STREET_UPDATE="STREET_UPDATE"
+//action
+const updateStreet=(street)=>{
+    return{
+    type:STREET_UPDATE,
+    payload:street,}
+}
+const reducer=(state=initialState,action)=>{
+    switch (action.type){
+        case STREET_UPDATE:
+            // return{
+            //     ...state,
+            //     address:{
+            //         ...state.address,
+            //         street:action.payload
+            //     }
+            // }
+            return produce(state,(draft)=>{
+                draft.address.street=action.payload     
+            })
+        default:{
+            return state
+        }
+    }
+}
+const store=redux.createStore(reducer)
+console.log("Initial State:",store.getState());
+const unSubscribe=store.subscribe(()=>{
+    console.log("Updated State:",store.getState());
+})
+store.dispatch(updateStreet("456 Main st"))//passsed as value to the update state function
+unSubscribe()

c1ed23caa44aa0d70a68154116fa67bd19999e6d
Arvind-ss <128106810+Arvind-ss@users.noreply.github.com>
2023-08-04 15:07:15 +0530

    04-08-23

--- index.js
+++ index.js
@@ -1,35 +1,46 @@
+const redux = require("redux");
+const createStore = redux.legacy_createStore;
+const CAKE_ORDERED = "cake_ordered";
 //ACTIONS:-
 // * actions are the only way applications can interact with the store
 // * carry some info from app to redux store
 // * plain javaScript object
 // * Have a type property that describes something that happened in the application
 
-const CAKE_ORDERED = "CAKE_ORDERED";
-//ActionCreator:a function which returns a action
-function orderCake() {
-  return;
-  {
-    type: CAKE_ORDERED;
-    quantity: 1;
-  }
-}
-//State
-const initialState = {
-  numOfCakes: 10,
-};
-//Reducer:-
-// * Reducer's Specify how the app's state changes inresponse to actions sent to the store
-// * Reducer is a Function which accepts state and function as argument and returns thre next state as a
+// //ActionCreator:a function which returns a action
 
-// (previosState,action)=>newState
+function order_cake() {
+  return {
+    //Action is passed as a method
+    type: "CAKE_ORDERED",
+    quantity: 1,
+  };
+}
+// //State
+
+const initialState = {
+  noOfCakes: 10,
+};
+// //Reducer:-
+// // * Reducer's Specify how the app's state changes inresponse to actions sent to the store
+// // * Reducer is a Function which accepts state and function as argument and returns thre next state as a
+// Reducer:- (previousState,action)=>new State
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case CAKE_ORDERED:
+    case "CAKE_ORDERED":
       return {
         ...state,
-        numOfCakes: state.numOfCakes - 1,
+        noOfCakes: state.noOfCakes - 1,
       };
     default:
       return state;
   }
 };
+const store = createStore(reducer); // store created and the initialStates have been saved in the store
+console.log("Initial State:", store.getState()); //since we have not performed any transsitions yet getState would provide
+const unSubscribe = store.subscribe(() =>
+  console.log("Update State:", store.getState())
+);
+store.dispatch(order_cake());
+store.dispatch(order_cake());
+unSubscribe();
